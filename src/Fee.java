91c9b44d0711a5944da8b01735f4e9b087e430fa
Zark247 <Zark247@users.github.com>
2019-10-26 22:49:03 -0400

    Modification to Fee class

--- src/Fee.java
+++ src/Fee.java
@@ -1,20 +1,35 @@
 package src;
 
 public class Fee {
-    private double max, fine, total;
 
-    public Fee() {
+    private double max, fine, total;
+    private Media media;
+
+    public Fee(Media aMedia) {
         total = 0.00;
         fine = 0.10;
         max = 15.00;
-
+        media = aMedia;
     }
+
     public void dailyFineIncrease() {
-        total+=fine;
+        if(total+fine > max) {
+            // System.out.println("Max fine reached");
+        } else {  // total + fine < max
+            total += fine;
+        }
     }
 
     public void pay(double amount) {
-        total-=amount;
+        double tempTotal = total;
+        if(amount > total) {
+            System.out.println("You cannot pay more than your current total."
+            +"\nTotal fees: " + tempTotal);
+        } else {  // amount <= total
+            total -= amount;
+            System.out.println("Total fees: " + tempTotal + " , Payment: " + amount
+            +"\nNew total after payment: "+ total);
+        }
     }
 
     public Media returnMedia() {
